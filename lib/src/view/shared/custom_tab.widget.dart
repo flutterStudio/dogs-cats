1a7f5f5309d0c97548094feaa0d96007c7a34a2b
SamirAhmad <samir.ahmad@datalineserver.com>
2022-04-22 17:32:18 +0300

    Create tab widget.

---
+++ lib/src/view/shared/custom_tab.widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CustomTabWidget extends StatelessWidget {
+  const CustomTabWidget({
+    Key? key,
+    required this.isActive,
+    required this.isDog,
+  }) : super(key: key);
+  final bool isActive;
+  final bool isDog;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SvgPicture.asset(
+          "assets/icons/${isDog ? 'dog' : 'cat'}${isActive ? '-active' : ''}.svg",
+          width: 70,
+          color: isActive
+              ? Theme.of(context).colorScheme.primaryVariant
+              : Theme.of(context).colorScheme.primary,
+        ),
+        Text(isDog ? "Dogs" : "Cats")
+      ],
+    );
+  }
+}
