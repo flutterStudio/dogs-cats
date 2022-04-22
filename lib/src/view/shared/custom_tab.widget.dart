c2c3cc6255472339ea709ee2321972bb55294738
SamirAhmad <samir.ahmad@datalineserver.com>
2022-04-22 17:51:57 +0300

    Add onClick callback to the tab widget.

--- lib/src/view/shared/custom_tab.widget.dart
+++ lib/src/view/shared/custom_tab.widget.dart
@@ -7,31 +7,36 @@ class CustomTabWidget extends StatelessWidget {
     Key? key,
     required this.isActive,
     required this.isDog,
+    required this.onClick,
   }) : super(key: key);
   final bool isActive;
   final bool isDog;
+  final VoidCallback onClick;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SvgPicture.asset(
-          "assets/icons/${isDog ? 'dog' : 'cat'}${isActive ? '-active' : ''}.svg",
-          width: 70,
-          color: isActive
-              ? Theme.of(context).colorScheme.primaryVariant
-              : Theme.of(context).colorScheme.primary,
-        ),
-        const SizedBox(height: 15),
-        Text(
-          isDog ? "Dogs" : "Cats",
-          style: Theme.of(context).textTheme.headline5!.copyWith(
-                fontFamily: 'Inder',
-                color: isActive
-                    ? Theme.of(context).colorScheme.primaryVariant
-                    : Theme.of(context).colorScheme.primary,
-              ),
-        )
-      ],
+    return GestureDetector(
+      onTap: onClick,
+      child: Column(
+        children: [
+          SvgPicture.asset(
+            "assets/icons/${isDog ? 'dog' : 'cat'}${isActive ? '-active' : ''}.svg",
+            width: 70,
+            color: isActive
+                ? Theme.of(context).colorScheme.primaryVariant
+                : Theme.of(context).colorScheme.primary,
+          ),
+          const SizedBox(height: 15),
+          Text(
+            isDog ? "Dogs" : "Cats",
+            style: Theme.of(context).textTheme.headline5!.copyWith(
+                  fontFamily: 'Inder',
+                  color: isActive
+                      ? Theme.of(context).colorScheme.primaryVariant
+                      : Theme.of(context).colorScheme.primary,
+                ),
+          )
+        ],
+      ),
     );
   }
 }
