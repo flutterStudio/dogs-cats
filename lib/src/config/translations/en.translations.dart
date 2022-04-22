9b78a55df29ae1f43931b00c4e392fa5dc55ef37
SamirAhmad <samir.ahmad@datalineserver.com>
2022-04-22 22:28:45 +0300

    Add errors translations.

--- lib/src/config/translations/en.translations.dart
+++ lib/src/config/translations/en.translations.dart
@@ -4,4 +4,19 @@ Map<String, String> _english = {
   "appTitle": "Flutter Template",
   "en": "English",
   "ar": "Arabic",
+  ..._errors,
+  ..._text
+};
+
+Map<String, String> _errors = {
+  "error-unknown": "Unknown error",
+  "error-timeout": "Timeout error",
+};
+
+Map<String, String> _text = {
+  "tryAgain": "Try again",
+  "dog": "Dog",
+  "dogs": "Dogs",
+  "cat": "Cat",
+  "cats": "Cats",
 };
