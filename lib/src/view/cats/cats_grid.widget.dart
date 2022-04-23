22403503818318732843bf32ac128e8675845fae
SamirAhmad <samir.ahmad@datalineserver.com>
2022-04-23 18:21:08 +0300

    Create cats grid view.

---
+++ lib/src/view/cats/cats_grid.widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_template/src/controller/home_screen.controller.dart';
+import 'package:flutter_template/src/model/cat.model.dart';
+import 'package:flutter_template/src/view/shared/data_handler.widget.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
+
+class CatsGrid extends GetView<HomeScreenController> {
+  const CatsGrid({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() => DataHandler<List<CatModel>>(
+        onErrorRetry: () {
+          controller.getDogs();
+        },
+        onSuccess: (context, data) => MasonryGridView.builder(
+              itemCount: data.length,
+              gridDelegate:
+                  const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemBuilder: (context, index) => Container(
+                color: Colors.accents[index],
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            ),
+        data: controller.cats));
+  }
+}
