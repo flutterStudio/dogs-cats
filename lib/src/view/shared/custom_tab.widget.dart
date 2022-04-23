aceeba510c75358d465c3b0eb74755653c5abb12
SamirAhmad <samir.ahmad@datalineserver.com>
2022-04-23 20:39:15 +0300

    convert tab's main axis to horizontal.

--- lib/src/view/shared/custom_tab.widget.dart
+++ lib/src/view/shared/custom_tab.widget.dart
@@ -16,29 +16,35 @@ class CustomTabWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onClick,
-      child: Column(
-        children: [
-          SvgPicture.asset(
-            "assets/icons/${isDog ? 'dog' : 'cat'}${isActive ? '-active' : ''}.svg",
-            width: 50,
-            color: isActive
-                ? Theme.of(context).colorScheme.primaryVariant
-                : Theme.of(context).colorScheme.primaryVariant.withOpacity(0.5),
-          ),
-          const SizedBox(height: 15),
-          Text(
-            isDog ? "Dogs" : "Cats",
-            style: Theme.of(context).textTheme.headline6!.copyWith(
-                  fontFamily: 'Inder',
-                  color: isActive
-                      ? Theme.of(context).colorScheme.primaryVariant
-                      : Theme.of(context)
-                          .colorScheme
-                          .primaryVariant
-                          .withOpacity(0.5),
-                ),
-          )
-        ],
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+            SvgPicture.asset(
+              "assets/icons/${isDog ? 'dog' : 'cat'}${isActive ? '-active' : ''}.svg",
+              width: 30,
+              color: isActive
+                  ? Theme.of(context).colorScheme.primaryVariant
+                  : Theme.of(context)
+                      .colorScheme
+                      .primaryVariant
+                      .withOpacity(0.5),
+            ),
+            const SizedBox(width: 15),
+            Text(
+              isDog ? "Dogs" : "Cats",
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    fontFamily: 'Inder',
+                    color: isActive
+                        ? Theme.of(context).colorScheme.primaryVariant
+                        : Theme.of(context)
+                            .colorScheme
+                            .primaryVariant
+                            .withOpacity(0.5),
+                  ),
+            )
+          ],
+        ),
       ),
     );
   }
